550ec7e5f48dbc7fd048677aa182071aea129bbd
miti <miti@phas.ubc.ca>
2025-06-06 14:22:31 -0700

    Removed the GZ_SIM_RESOURCE_PATH initialization script (it is now in the launch file)

--- source_gazebo.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Exporting GZ_SIM_RESOURCE_PATH"
-export GZ_SIM_RESOURCE_PATH=/home/fizzer/enph353_ws/src/lab3_ros/models:/home/fizzer/enph353_ws/src/lab3_ros/media:/home/fizzer/enph353_ws/src/lab3_ros/worlds
